4ce1281f06030c3cd6234b7c219ed32998a659c7
fkadeal <96823972+F21deal@users.noreply.github.com>
2022-04-22 22:51:51 -0800

    setting app.js , add test bg color

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {View , Text, StyleSheet} from "react-native";
+
+class App extends React.Component{
+    render(){
+        return(
+            <View style={styles.container}>
+                 
+            </View>
+        ) 
+    }
+}
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor:'#dddf',
+        flex:1,
+
+    }
+})
+
+export default App;
