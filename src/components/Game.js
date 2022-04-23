58d882bd272780edab8e06467ff3261850842bdd
fkadeal <96823972+F21deal@users.noreply.github.com>
2022-04-23 00:58:55 -0800

    add prop to <Game /> , gernerate random nombers

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,12 +1,21 @@
 import React from "react";
 import {View , Text, StyleSheet} from "react-native";
-
+import  PropTypes from "prop-types";
 class Game extends React.Component{
-    target = 0 + Math.floor(10 * Math.random());
+    static PropTypes={
+        randomNumberCount: PropTypes.number.isRequired,
+    };
+    randomNumbers = Array.from({length:this.props.randomNumberCount})
+    .map(() => 1 + Math.floor(10 * Math.random()) );
+    target = this.randomNumbers.slice(0,this.props.randomNumberCount -2)
+    .reduce((acc, curr) => acc + curr,0);
+
     render(){
         return(
             <View style={styles.container}> 
             <Text style={styles.target}> {this.target}</Text>
+             {this.randomNumbers.map((randomNumber, index) =>
+                 <Text key={index}>{randomNumber}</Text> )} 
             </View>
         ) 
     }
