dc794d9b567010ac451340e7f1c7fc46bc34c7cb
fkadeal <96823972+F21deal@users.noreply.github.com>
2022-04-22 23:13:35 -0800

    add eslint config and install dev dependency

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,30 @@
 module.exports = {
+  parser:'babel-eslint',
+  env:{
+    browser:true,
+    commonjs:true,
+    es6:true,
+    node:true,
+    jest:true,
+  },
+  extends: ['eslint:recommended','plugin:react/recommended','@react-native-community'],
+  parserOptions:{
+    ecmaFeatures:{
+      experimentalObjectRestSpread:true,
+      jsx:true,
+    },
+    sourceType:'module',
+  },
+
+  plugins:['react'],
+  rules:{
+    'react/prop-types':['off'],
+    ident:['error',2,{SwitchCase:1}],
+    'linebreak-style':['error','unix'],
+    quotes:['error', 'single'],
+    semi:['error','always'],
+    'no-console':['warn',{allow:['info','error']}],
+    'arrow-parens':['error','always'],
+  },
   root: true,
-  extends: '@react-native-community',
 };
